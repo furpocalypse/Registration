bf96700e7497d8fa6efb8d25ac994bfc192d6241
Wes <wes@anthronewengland.com>
2023-06-07 00:57:38 -0400

    Remove some unused code

--- js/src/features/checkout/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { CheckoutComponentStateContext } from "#src/features/checkout/types/CheckoutComponentState.js"
-import { CheckoutStateContext } from "#src/features/checkout/types/CheckoutState.js"
-import { useContext } from "react"
-
-export const useCheckoutState = () => useContext(CheckoutStateContext)
-
-export const useCheckoutComponentState = () =>
-  useContext(CheckoutComponentStateContext)
